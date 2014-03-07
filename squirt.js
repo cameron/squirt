f21bac4d0b1522a38003151d2aac4b311e502b62
Cameron Boehmer <cameron.boehmer@gmail.com>
2014-03-06 21:51:07 -0800

    updated sources from master

--- squirt.js
+++ squirt.js
@@ -218,7 +218,7 @@
 
     var obscure = makeDiv({class: 'sq-obscure'}, squirt);
     on(obscure, 'click', function(){
-      window.squirted = null;
+      window.squirted = true;
       dispatch('squirt.close');
     });
 
