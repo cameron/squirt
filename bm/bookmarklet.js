397dd053d342ed7416b76066baff3fe8f1f9fbac
Cameron Boehmer <cameron.boehmer@gmail.com>
2014-03-10 15:22:17 -0700

    console log? no

--- bm/bookmarklet.js
+++ bm/bookmarklet.js
@@ -11,7 +11,6 @@
     if(s.idx != -1){
       s.ampIdx = s.search.indexOf('&');
       s.host = s.search.substring(s.idx + 7, s.ampIdx == -1 ? s.search.length : s.ampIdx);
-      console.log(s.host);
       s.src = '//' + (s.host ? s.host : 'localhost') + ':4000/bm/squirt.js';
     }
     document.body.appendChild(s);
