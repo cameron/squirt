b589afe9a66b43ec6c623165ce6b85858492e453
Cameron Boehmer <cameron.boehmer@gmail.com>
2014-03-12 03:48:42 -0700

    configure squirt.io to use prod analytics project in prod

--- js/io.js
+++ js/io.js
@@ -47,10 +47,16 @@
     window.Keen=window.Keen||{configure:function(e){this._cf=e},addEvent:function(e,t,n,i){this._eq=this._eq||[],this._eq.push([e,t,n,i])},setGlobalProperties:function(e){this._gp=e},onChartsReady:function(e){this._ocrq=this._ocrq||[],this._ocrq.push(e)}};(function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src=("https:"==document.location.protocol?"https://":"http://")+"dc8na2hxrj29i.cloudfront.net/code/keen-2.1.0-min.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 
     var Keen = window.Keen;
-    Keen.configure({
+    var prod = {
+      projectId: "531d7ffd36bf5a1ec4000000",
+      writeKey: "9bdde746be9a9c7bca138171c98d6b7a4b4ce7f9c12dc62f0c3404ea8c7b5415a879151825b668a5682e0862374edaf46f7d6f25772f2fa6bc29aeef02310e8c376e89beffe7e3a4c5227a3aa7a40d8ce1dcde7cf28c7071b2b0e3c12f06b513c5f92fa5a9cfbc1bebaddaa7c595734d"
+    };
+    var dev = {
       projectId: "531aa8c136bf5a0f8e000003",
       writeKey: "a863509cd0ba1c7039d54e977520462be277d525f29e98798ae4742b963b22ede0234c467494a263bd6d6b064413c29cd984e90e6e6a4468d36fed1b04bcfce6f19f50853e37b45cb283b4d0dfc4c6e7a9a23148b1696d7ea2624f1c907abfac23a67bbbead623522552de3fedced628"
-    });
+    };
+
+    Keen.configure(window.location.hostname.match('squirt.io') ? prod : dev);
 
     function addon(name, input, output){
       return { name: name, input: input, output: output};
