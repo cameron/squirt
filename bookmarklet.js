87f5fdba62bda2dcc38623ebba36eae2a5836775
Cameron Boehmer <cameron.boehmer@gmail.com>
2014-03-16 20:55:15 -0700

    protocol agnostic bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,13 +5,13 @@
     window.sq = {};
     window.sq.userId = '--squirtUser--';
     s = document.createElement('script');
-    s.src = 'http://www.squirt.io/bm/squirt.js';
+    s.src = '//squirt.io/bm/squirt.js';
     s.s = window.location.search;
     s.idx = s.s.indexOf('sq-dev');
     if(s.idx != -1){
       s.ampIdx = s.s.indexOf('&', s.idx);
       s.host = s.s.substring(s.idx + 7, s.ampIdx == -1 ? s.s.length : s.ampIdx);
-      s.src = 'http://' + (s.host ? s.host : 'localhost') + ':4000/bm/squirt.js';
+      s.src = '//' + (s.host ? s.host : 'localhost') + ':4000/bm/squirt.js';
     }
     document.body.appendChild(s);
   }
